80bcaa7c502297e5654127811e6a29e2a88155a8
Pasa <pavlo1191@gmail.com>
2025-04-15 04:46:21 +0200

    corrected background

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	fs := http.FileServer(http.Dir("static/css"))
-	http.Handle("/static/css/", http.StripPrefix("/static/css/", fs))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, nil)
