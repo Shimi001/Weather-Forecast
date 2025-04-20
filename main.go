2f2e8621a6f52b09d14723588173f152ec06df8e
Pasa <pavlo1191@gmail.com>
2025-04-20 13:24:00 +0200

    default2

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"net/http"
+	"os"
+	"text/template"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		panic(".env file not found")
+	}
+
+	// Set up a simple web server
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Main page
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, nil)
+	})
+
+	// Weather API endpoint
+	http.HandleFunc("/weather", func(w http.ResponseWriter, r *http.Request) {
+		apiKey := os.Getenv("API_KEY")
+		if apiKey == "" {
+			http.Error(w, "API_KEY not set in .env file", http.StatusInternalServerError)
+			return
+		}
+	})
+
+	http.ListenAndServe(":80", nil)
+}
