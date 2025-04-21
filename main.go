8610183b17f22e85f2db87faf4e30309b07856d3
Pasa <pavlo1191@gmail.com>
2025-04-21 13:07:01 +0200

    default3

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"text/template"
@@ -10,7 +12,9 @@ import (
 )
 
 type WeatherData struct {
-	City string `json:"city"`
+	City string  `json:"city"`
+	Temp float64 `json:"temp"`
+	Desc string  `json:"desc"` // Weather description
 }
 
 func main() {
@@ -32,20 +36,51 @@ func main() {
 
 	// Weather API endpoint
 	http.HandleFunc("/weather", func(w http.ResponseWriter, r *http.Request) {
+		// Get 'city' query parameter from the URL
+		city := r.URL.Query().Get("city")
+		if city == "" {
+			http.Error(w, "City parameter is required", http.StatusBadRequest)
+			return
+		}
+
+		// Set apiKey from environment variable
 		apiKey := os.Getenv("API_KEY")
 		if apiKey == "" {
 			http.Error(w, "API_KEY not set in .env file", http.StatusInternalServerError)
 			return
 		}
 
-		сity := r.URL.Query().Get("city")
-		if сity == "" {
-			http.Error(w, "City parameter is required", http.StatusBadRequest)
+		// OpenWeatherMap API URL
+		apiURL := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric", city, apiKey)
+		resp, err := http.Get(apiURL)
+		if err != nil {
+			http.Error(w, "Failed to get weather", http.StatusInternalServerError)
 			return
 		}
+		defer resp.Body.Close()
 
+		// Read the response body from OpenWeatherMap
+		body, _ := io.ReadAll(resp.Body)
+
+		// Create a struct to hold the API response
+		var apiResp struct {
+			Name string `json:"name"`
+			Main struct {
+				Temp float64 `json:"temp"`
+			} `json:"main"`
+			Weather []struct {
+				Description string `json:"description"`
+			} `json:"weather"`
+		}
+
+		// Decode JSON response into the apiResp struct
+		json.Unmarshal(body, &apiResp)
+
+		// Data of the weather
 		data := WeatherData{
-			City: сity,
+			City: apiResp.Name,
+			Temp: apiResp.Main.Temp,
+			Desc: apiResp.Weather[0].Description,
 		}
 
 		w.Header().Set("Content-Type", "application/json")
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,8 +3,10 @@ function getWeather(city) {
     .then(response => response.json())
     .then(data => {
         document.getElementById("city").innerText = data.city;
+        document.getElementById("temp").innerText = data.temp + "°C";
+        document.getElementById("desc").innerText = data.desc;
     })
     .catch(error => console.error("Error fetching weather data:", error));
 }
 
-getWeather("London");
+getWeather("Kyiv");
