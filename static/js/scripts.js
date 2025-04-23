4ad5c54273f61c3763d18a4d60f1b8ab65676c20
Pasa <pavlo1191@gmail.com>
2025-04-23 22:08:25 +0200

    template

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,12 +1,17 @@
+// const cityElement = document.querySelector('.city-name');
+
 function getWeather(city) {
     fetch(`/weather?city=${city}`)
     .then(response => response.json())
     .then(data => {
         document.getElementById("city").innerText = data.city;
-        document.getElementById("temp").innerText = data.temp + "°C";
+        document.getElementById("temp").innerText = data.temp;
         document.getElementById("desc").innerText = data.desc;
     })
     .catch(error => console.error("Error fetching weather data:", error));
 }
 
-getWeather("Kropyvnytskyi");
+const cityName = "Kropyvnytskyi";
+// cityElement.textContent = cityName;
+
+getWeather(cityName);
